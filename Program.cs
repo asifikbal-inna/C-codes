3c46ecc536599f6dc04468618cc802ac6d0e4720
Md Asif Ikbal <75169639+asifikbal-inna@users.noreply.github.com>
2023-02-05 05:20:21 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class program
+{
+    static void Main1()
+    {
+        Console.WriteLine("Welcome to C sharp training  1");
+    }
+    static void Main()
+    {
+        Console.WriteLine("Welcome to C sharp training ");
+        Main1();
+    }
+
+}
