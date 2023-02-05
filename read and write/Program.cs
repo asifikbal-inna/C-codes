4a5beb9ba6cbb9b55b3b7595ce023582ab0b4739
Md Asif Ikbal <75169639+asifikbal-inna@users.noreply.github.com>
2023-02-05 14:29:25 +0600

    Add files via upload

---
+++ read and write/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void main()
+    {
+        Console.WriteLine(" PLEASE ENTER YOUR NAME");
+        string Su = Console.ReadLine();
+        Console.WriteLine("Hello " + Su);
+    }
+
+}
---
+++ readndwrite/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(" PLEASE ENTER YOUR NAME");
+         string Su = Console.ReadLine();
+         Console.WriteLine("Hello " + Su);
+        Console.WriteLine("hello {0}", Su);
+        Console.WriteLine("Enter your age");
+
+        string suu = Console.ReadLine();
+        Console.WriteLine("Your age is {0}, {1} ", suu,Su);
+
+
+
+
+    }
+
+}
+
